843f567ceee95632d82729bee417767ca5f202b2
seoeongeueun <92116454+seoeongeueun@users.noreply.github.com>
2021-10-22 13:30:25 -0400

    data post to db
    localhost:3000/api/quizzes/seed worked

--- server/routers/quizRouter.js
+++ server/routers/quizRouter.js
@@ -8,10 +8,11 @@ const quizRouter = express.Router();
 quizRouter.get('/seed', 
     expressAsyncHandler(async(req, res) => {
         //await Quiz.remove({})
-        const createdQuizzes = data.quizzes
+        const createdQuizzes = await Quiz.insertMany(data.quizzes)
         res.send({ createdQuizzes });
     })
-);
+)
+
 //quizRouter.put('/quiz/:id', QuizCtrl.updateQuiz)
 //quizRouter.get('/quiz/:nid', QuizCtrl.getQuizById)
 //quizRouter.get('/quizzes', QuizCtrl.getQuizzes)
