194bd812e4a0dfa1e89fed08782a40df38774359
Kevin Mai <kevin.mai.1@stonybrook.edu>
2021-10-30 23:30:57 -0400

    working login

--- server/server.js
+++ server/server.js
@@ -10,84 +10,79 @@ import { exec } from "child_process";
 import questionRouter from "./routers/questionRouter.js";
 import platformRouter from "./routers/platformRouter.js";
 
-
 env(); // set enviornment variables
 const app = express();
 const __dirname = path.resolve();
 const ORIGIN = process.env.ORIGIN ? process.env.ORIGIN : "127.0.0.1:3000";
 const MONGODB_URL = process.env.MONGODB_URL
-    ? process.env.MONGODB_URL
-    : "mongodb://localhost/bluespace";
-
+  ? process.env.MONGODB_URL
+  : "mongodb://localhost/bluespace";
 
 //just gets current user
 app.get("/me", async (req, res) => {
   res.status(200);
   res.json(req.user);
 });
-    
+
 // app.use(logger("dev"));
 //localhost:3000/static
 app.use("/githubwebhook", (req, res) => {
-    exec("cd ./client/ && npm run build"); 
-    res.status(200);
+  exec("cd ./client/ && npm run build");
+  res.status(200);
 });
 
-
-
-
 app.use(
-    "/static",
-    express.static(path.join(__dirname, "./client/build/static"))
+  "/static",
+  express.static(path.join(__dirname, "./client/build/static"))
 );
 app.use(
-    "/images",
-    express.static(path.join(__dirname, "./client/build/images"))
+  "/images",
+  express.static(path.join(__dirname, "./client/build/images"))
 );
 app.use(express.json({ limit: "50mb" }));
 app.use(
-    express.urlencoded({
-        limit: "50mb",
-        extended: true,
-        parameterLimit: 50000,
-    })
+  express.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
 );
 app.use(cookieParser());
 app.use(
-    cors({
-        origin: ORIGIN,
-        credentials: true,
-        optionsSuccessStatus: 200,
-    })
+  cors({
+    origin: ORIGIN,
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
 );
 
 app.use("/", function (req, res, next) {
-    //localhost:3000/api/v1/request
-    if (req.path.indexOf("api") != -1) return next();
-    res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+  //localhost:3000/api/v1/request
+  if (req.path.indexOf("api") != -1) return next();
+  res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
 });
 
 mongoose
-    .connect(MONGODB_URL, {
-        useNewUrlParser: true,
-    })
-    .catch((e) => {
-        console.error("Connection error", e.message);
-    });
+  .connect(MONGODB_URL, {
+    useNewUrlParser: true,
+  })
+  .catch((e) => {
+    console.error("Connection error", e.message);
+  });
 
 app.use("/api/quizzes", quizRouter);
-app.use('/api/questions', questionRouter);
+app.use("/api/questions", questionRouter);
 app.use("/api/platform", platformRouter);
 app.use("/api/v1", userRouter);
 app.get("/", (req, res) => {
-    res.send("Server is Ready");
+  res.send("Server is Ready");
 });
 
 app.use((err, req, res, next) => {
-    res.status(500).send({ message: err.message });
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Serve at http://localhost:${port}`);
+  console.log(`Serve at http://localhost:${port}`);
 });
