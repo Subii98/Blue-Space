0322dbabcb8c0c3fac5bbe8f71e649b9ff9d05b0
Kevin Mai <kevin.mai.1@stonybrook.edu>
2021-10-30 22:56:42 -0400

    tried to pull

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,15 @@
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-import Header from './components/Header.js'
-import HomeScreen from './screens/HomeScreen.js';
-import QuizScreen from './screens/QuizScreen.js';
-import createQuiz from './screens/createQuiz';
-import PlatformScreen from './screens/PlatformScreen.js';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./components/header.js";
+import HomeScreen from "./screens/HomeScreen.js";
+import QuizScreen from "./screens/QuizScreen.js";
+import createQuiz from "./screens/createQuiz";
+import PlatformScreen from "./screens/PlatformScreen.js";
 import Login from "./components/Login.js";
 import Logout from "./components/Logout.js";
-import createUser from "./components/CreateUser.js";
-//import { createContext, userState } from "react";
-//import { Provider } from "react-redux";
-
+import CreatePlatform from "./screens/CreatePlatform.js";
 
 function App() {
-  /*
-  const UserContext = React.createContext("");
-  const EmailContext = React.createContext("light");
-  const LoggedState = React.createContext("light");
-  const ThemeContext = React.createContext("light");
-  */
   return (
     <BrowserRouter>
       <div className="grid-container">
@@ -28,9 +19,9 @@ function App() {
           <Route path="/quiz/:id" component={QuizScreen}></Route>
           <Route path="/quizCreate" component={createQuiz}></Route>
           <Route path="/platform" component={PlatformScreen}></Route>
-          <Route path="/createuser" component={createUser}></Route>
+          <Route path="/CreatePlatform" component={CreatePlatform}></Route>
         </main>
-      </div> 
+      </div>
 
       <Login />
       <Logout />
--- client/src/components/PlatformListArea.js
+++ client/src/components/PlatformListArea.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function PlatformListArea() {
-    return (
-      <div className="postarea">
-        <div className="platformListArea">
-          <div className="sectionTitle">
-            <span> Quiz for Platforms</span>
-            <button type="button">
-              <Link to="/quizCreate">Create new quiz</Link>
-            </button>
-          </div>
+  return (
+    <div className="postarea">
+      <div className="platformListArea">
+        <div className="sectionTitle">
+          <span> Quiz for Platforms</span>
+          <button type="button">
+            <Link to="/quizCreate">Create new quiz</Link>
+          </button>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
-export default PlatformListArea
+export default PlatformListArea;
