819a20ecbc6076eb94d8fb4dc592fc8a5e999d6a
Kevin Mai <kevin.mai.1@stonybrook.edu>
2021-11-11 23:10:04 -0500

    added a routing for categories

--- client/src/components/CategoriesCard/Categories.js
+++ client/src/components/CategoriesCard/Categories.js
@@ -2,17 +2,26 @@ import React from 'react'
 import CategoryCard from './CategoryCard.js'
 
 
+
 function Categories() {
-    const lizardInfo = {name:"Lizzard", description: "Lizards from all sizes are included. Lizards are one the most amazing creatures on earth",image: "./images/lizard.jpg",route: "/"};
-    const catinfo = [{name: "BlueSPace", description :"Quiz on blue space the website", 
-    image: "./images/logo11.png",route: "/"},lizardInfo]
+    const lizardInfo = {name:"Lizzard", description: "Lizards from all sizes are included. Lizards are one the most amazing creatures on earth",image: "./images/lizard.jpg",route: "/search", search: 'lizzard'};
+    const bluespace = {name: "BlueSPace", description :"Quiz on blue space the website", 
+    image: "./images/logo11.png",route: "/search", search: 'bluespace'};
+    const filler = {name: "Filler", description :"Will search for test", image: "./images/logo11.png",route: "/search", search: "test"};
+    const catinfo = [bluespace,lizardInfo,filler]
     return (
-        <div>
-            {catinfo.map((elem) => (
-                <CategoryCard category={elem} />
-            ))}
-        </div>
-    )
+      <div
+        className={['flex-container', 'wrap'].join(
+          " "
+        )}
+      >
+        {catinfo.map((elem) => (
+          <div className='flex-item'>
+            <CategoryCard category={elem} />
+          </div>
+        ))}
+      </div>
+    );
 }
 
 export default Categories
--- client/src/components/CategoriesCard/CategoryCard.js
+++ client/src/components/CategoriesCard/CategoryCard.js
@@ -5,9 +5,11 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { GlobalStoreContext } from "../../store";
+import { useContext, useEffect, useState } from "react";
 
 function CategoryCard(props) {
-
+  const { store } = useContext(GlobalStoreContext);
     /*
 <Typography variant="subtitle1" color="text.secondary" component="div">
             {props.user}
@@ -19,7 +21,7 @@ function CategoryCard(props) {
     return (
         <div>
             <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea component={Link} to={props.category.route}>
+      <CardActionArea onClick={()=> {store.setSearch(props.category.search)}}component={Link} to={props.category.route}>
         <CardMedia
           component="img"
           height="140"
