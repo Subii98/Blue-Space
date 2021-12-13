dbce6dbac636e4054f865be2b85b290cc2040813
Subii98 <92116531+Subii98@users.noreply.github.com>
2021-12-13 15:19:42 -0500

    owner only create quiz

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -7,14 +7,38 @@ import { useIsMounted } from "../components/useIsMounted.js";
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
 import { useHistory } from "react-router-dom";
+import { prop } from "dom7";
 
 function Quiz(props) {
     const [quizId, setQuizId] = useState();
     const [quizzes, setQuizzes] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [isOwner, setIsOwner] = useState(false);
+    const [platform, setPlatform] = useState();
+    const [user, setUser] = useState();
     const isMounted = useIsMounted();
     const history = useHistory();
+
+    useEffect(()=> {
+        if(user&&platform){
+            if (user._id == platform.userId) {
+                setIsOwner(true);
+            }
+        }
+        console.log("is owner?", isOwner)
+    })
+
+    function fetchUser() {
+        let userData = localStorage.getItem("data");
+        userData = JSON.parse(userData);
+        axios
+            .get("/api/v1/get_user?user_id=" + userData.id)
+            .then(res => setUser(res.data))
+            .catch(error => {
+                setError("No userdata");
+            });
+    }
 
     useEffect(() => {
         isMounted.current = true;
@@ -24,9 +48,25 @@ function Quiz(props) {
     }, []);
 
     useEffect(() => {
+        if (props.platformId) {
+            fetchPlatform();
+        }
+        fetchUser();
         if (props.platformId) fetchQuiz();
     }, [props.platformId]);
 
+    function fetchPlatform() {
+        
+        axios
+        .get('/api/platforms/by_id/' + props.platformId)
+        .then(res =>{
+            setPlatform(res.data);
+            return;
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    }
     function fetchQuiz() {
         axios
             .get("/api/quizzes/" + props.platformId)
@@ -56,7 +96,9 @@ function Quiz(props) {
                 <div className="platformQuiz">
                     <div className="quizHeader">
                         <p>Quiz</p>
-                        <Button onClick={() => history.push("/CreateQuiz/" + props.platformId)}>
+                        <Button 
+                        style={isOwner ? {}:{display:"none"}}
+                        onClick={() => history.push("/CreateQuiz/" + props.platformId)}>
                             Create
                         </Button>
                     </div>
